56e7852ecff8d9314769a6439c56b94463c52303
Harshikavrm <harshikav58@gmail.com>
2025-08-10 23:31:16 +0530

    Initial commit

--- number_Guessing_Game/main.py
+++ number_Guessing_Game/main.py
@@ -1,1 +1,21 @@
+import random
+    #Logic Building
+# Program picks a random number
+secret_number = random.randint(1, 100)
+# The user guesses the number
+print("Welcome to guessing the number game")
 
+#The program tells them if their guess is too high, too low, or correct.
+while True:
+    try:
+        user_Guess = int(input("Guess a number between 1 to 100:"))
+        if user_Guess > secret_number:
+            print("Number is too high")
+        elif user_Guess < secret_number:
+            print("Number is too low")
+        else:
+            print("Number is correct.")
+            break
+    except ValueError:
+        print("Input the correct number.")
+#It keeps going until the user guesses correctly(and often tracks how many attempts they took)
